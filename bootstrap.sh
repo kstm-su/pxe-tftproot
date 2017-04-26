3a0d3b1c31ebf3bf66ab949180b087ed3f328b29
bgpat <bgpat23@gmail.com>
2017-04-26 10:30:37 +0900

    Create empty config file for each dist

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,4 +2,5 @@ mkdir tmp
 wget https://www.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.tar.gz -O tmp/syslinux.tar.gz
 tar xzvf tmp/syslinux.tar.gz -C tmp
 mv tmp/syslinux-6.03 syslinux
+grep -v '^$' dists.txt | awk '$0="pxelinux.cfg/"$1".cfg"' | sort | uniq | xargs touch
 cat dists.txt | while read line; do ./append.sh $line; done
