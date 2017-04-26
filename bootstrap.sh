147bc65aa922811a9b183bc4ec1f2d8c005c27b7
bgpat <bgpat23@gmail.com>
2017-04-26 22:41:41 +0900

    Add HDD tool

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,5 @@ tar xzvf tmp/syslinux.tar.gz -C tmp
 mv tmp/syslinux-6.03 syslinux
 grep -v '^$' dists.txt | awk '$0="pxelinux.cfg/"$1".cfg"' | sort | uniq | xargs touch
 cat dists.txt | while read line; do ./append.sh $line; done
+mkdir tools
+wget http://www.hgst.com/sites/default/files/downloads/dft32_v416_b00_install.IMG -O tools/dft.img
